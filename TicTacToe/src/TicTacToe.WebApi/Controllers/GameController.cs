57c7ab90a2c9ea7c95bb9c5cab2a46774258d336
Pawel  Jelinski <jelinski89@gmail.com>
2018-02-10 14:18:47 +0100

    added acceptance test and GameController

---
+++ TicTacToe/src/TicTacToe.WebApi/Controllers/GameController.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TicTacToe.WebApi.Controllers
+{
+    public class GameController : Controller
+    {
+        [HttpPost, Route("game/new")]
+        public async Task<IActionResult> CreateNewGame()
+        {
+            return Ok("{[_,_,_,_,_,_,_,_,_]}");
+        }
+    }
+}
---
+++ TicTacToe/test/TicTacToe.WebApi.Tests/GameControllerTests.cs
@@ -0,0 +1,29 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using NUnit.Framework;
+
+namespace TicTacToe.WebApi.Tests
+{
+    [TestFixture]
+    public class GameControllerTests
+    {
+        [Test]
+        public async Task NewGame_ReturnsEmptyBoard()
+        {
+            var webHostBuilder = new WebHostBuilder().UseStartup<Startup>();
+            using (var testServer = new TestServer(webHostBuilder))
+            {
+                var client = testServer.CreateClient();
+                var response = await client.PostAsync("game/new", new StringContent(""));
+                var responseContent = await response.Content.ReadAsStringAsync();
+                var expectedJson = "{[_,_,_,_,_,_,_,_,_]}";
+
+                Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+                Assert.That(responseContent, Is.EqualTo(expectedJson));
+            }
+        }
+    }
+}
