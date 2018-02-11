1d585f25310b77e6fd04b59c48dd8ecd88608a46
Pawel  Jelinski <jelinski89@gmail.com>
2018-02-11 21:49:51 +0100

    refactoring

--- TicTacToe/src/TicTacToe.WebApi/Models/Board.cs
+++ TicTacToe/src/TicTacToe.WebApi/Models/Board.cs
@@ -1,40 +1,33 @@
-﻿using TicTacToe.WebApi.Exceptions;
+﻿using System.Collections.Generic;
+using System.Linq;
+using TicTacToe.WebApi.Exceptions;
 
 namespace TicTacToe.WebApi.Models
 {
     public class Board
     {
         private readonly CellState[] _cells;
-        private CellState _nextMove;
+        private readonly Dictionary<CellState, string> _cellStateMap;
 
         public Board()
         {
-            _nextMove = CellState.O;
             _cells = new CellState[9];
+            _cellStateMap = new Dictionary<CellState, string>
+            {
+                {CellState.O, "o"},
+                {CellState.X, "x"},
+                {CellState.Empty, "_"}
+            };
         }
 
         public string Serialize()
         {
-            var cellString = new string [9];
+            var mappedCells = _cells.Select(MapToString);
 
-            for (var i = 0; i < _cells.Length; i++)
-            {
-                if (_cells[i] == CellState.O)
-                {
-                    cellString[i] = "o";
-                }
-                else if (_cells[i] == CellState.X)
-                {
-                    cellString[i] = "x";
-                }
-                else
-                {
-                    cellString[i] = "_";
-                }
-            }
-            
-            return $"{{[{string.Join(",", cellString)}]}}";
+            return $"{{[{string.Join(",", mappedCells)}]}}";
         }
+
+        private string MapToString(CellState cellState) => _cellStateMap[cellState];
 
         public void MakeMove(uint i)
         {
@@ -43,16 +36,11 @@ namespace TicTacToe.WebApi.Models
                 throw new InvalidMoveException();
             }
 
-            if (_nextMove == CellState.O)
-            {
-                _cells[i] = CellState.O;
-                _nextMove = CellState.X;
-            }
-            else
-            {
-                _cells[i] = CellState.X;
-                _nextMove = CellState.O;
-            }
+            _cells[i] = GetNextCellState();
         }
+
+        private CellState GetNextCellState() => IsTicTheNextMove() ? CellState.O : CellState.X;
+
+        private bool IsTicTheNextMove() => _cells.Count(c => c != CellState.Empty) % 2 == 0;
     }
 }
